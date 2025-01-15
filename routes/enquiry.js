4e07c549cbe05ec7a60dfe884857212adb6d1172
shiraznazir <shiraznazir786@gmail.com>
2025-01-15 05:36:36 +0530

    add getting data in enquiry

--- routes/enquiry.js
+++ routes/enquiry.js
@@ -27,6 +27,17 @@ async function generateUniqueCallId() {
   return callId;
 }
 
+router.get('/', (req, res)=>{
+  try {
+    res.status(201).json({
+      message: 'Getting data from server',
+      status: true,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error saving customer data', error: err.message });
+  }
+})
+
 // Create a new customer
 router.post('/', upload.none(), async (req, res) => {
   const { name, email, mobileNumber, pincode, message, address, type } = req.body;
