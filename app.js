bb0c343fed0741ba14f6dbe76c1e3fb20ec131ae
shiraznazir <shiraznazir786@gmail.com>
2026-03-05 01:08:34 +0530

    resolve cors issue

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.disable("x-powered-by"); // Hide framework information
 const allowedOrigins = [
     "https://www.servicewalah.in",
     "https://sw-backend-2o0u.onrender.com/",
-    "https://servicewalah.com",
+    "https://servicewalah.in",
+    "https://api.servicewalah.in",
     "http://localhost:3000",
     "http://localhost:8080",
     "http://localhost:3001",
